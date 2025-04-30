61aa204923114d23fb74587ee93c5a5ebec0616f
Bryce Hall <brycehall2213@gmail.com>
2025-04-30 16:21:01 -0400

    feat: lambda

---
+++ .github/workflows/lambda.tf
@@ -0,0 +1,10 @@
+resource "aws_lambda_function" "brycehall_lambda" {
+  function_name = "brycehall_lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  runtime       = "python3.12"
+  handler       = "test_script.lambda_handler"
+  filename      = "${path.module}/lambda_package.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda_package.zip")
+
+  timeout = 10
+}
